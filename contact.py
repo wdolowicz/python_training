e5f702d3b6fca7a24f7b9acbf98bbe11401e54ea
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-10-31 20:03:20 +0100

    Auxiliary class created

---
+++ contact.py
@@ -0,0 +1,18 @@
+__author__ = 'wdolowicz'
+
+
+class Contact:
+
+    def __init__(self, name, initials, lname, nick, title, company, address, hphone, mail, web, byear, ayear):
+        self.name = name
+        self.initials = initials
+        self.lname = lname
+        self.nick = nick
+        self.title = title
+        self.company = company
+        self.address = address
+        self.hphone = hphone
+        self.mail = mail
+        self.web = web
+        self.byear = byear
+        self.ayear = ayear

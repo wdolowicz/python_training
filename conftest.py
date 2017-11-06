f5852d4c886471a4e06b01e3c6037865ef8d01bc
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-06 22:53:26 +0100

    Fixture converted to global

---
+++ conftest.py
@@ -0,0 +1,10 @@
+__author__ = 'wdolowicz'
+
+import pytest
+from fixture.application import Application
+
+@pytest.fixture(scope = "session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture

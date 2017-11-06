8f726a67ec8e1244a8c94494fca0374398be57f0
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-06 22:54:07 +0100

    Removed fixture reference

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "wdolowicz"
-import pytest
-from fixture.application import Application
+
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):

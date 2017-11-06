81a3c40a317a07f245da10bf5d2b1d3f16154efc
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-06 21:25:01 +0100

    Extracted GroupHelper Class

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,11 +15,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Homework group", header="blabla", footer="blabl"))
+    app.group.create(Group(name="Homework group", header="blabla", footer="blabl"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()

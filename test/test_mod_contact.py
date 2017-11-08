1c90399d782ddb2ff3d69d275db5823c81caac52
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-08 18:45:10 +0100

    Created test for contact modification

---
+++ test/test_mod_contact.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = "wdolowicz"
+
+from model.contact import Contact
+
+
+def test_mod_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modcontact(Contact(name="Carl", initials="CJ", lname="Johnson", nick="CJ", title="Mr", company="Rockstar Games",
+                                   address="Grove Street", hphone="+199988877715", mail="cj@rockstar.com",
+                                   web="www.rockstargames.com"))
+    app.session.logout()

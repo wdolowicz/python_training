4dcceaac2ddd6af937a9639e33e44a190ed18079
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-15 19:11:46 +0100

    Added and removed logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,6 +7,5 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="Homework group", header="blabla", footer="blabl"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))

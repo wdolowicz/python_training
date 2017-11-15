0e6fa76c84a807e8061546d46e286465a3a1908c
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-15 18:46:44 +0100

    Removed login/logout

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,18 +5,14 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name="John", initials="JD", lname="Doe", nick="JD", title="Mr", company="Microsoft",
                                address="Redmond", hphone="+165688877744", mail="johndoe@microsoft.com",
                                web="www.johndoe.com"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name="", initials="", lname="", nick="", title="", company="",
                                address="", hphone="", mail="", web=""))
-    app.session.logout()
 
 
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,6 +2,4 @@ __author__ = "wdolowicz"
 
 
 def test_del_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
-    app.session.logout()

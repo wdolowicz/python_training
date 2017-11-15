33a0f51228498f78fff2c1a5e9afe342da527c8e
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-15 22:45:26 +0100

    Removed wait time

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,6 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)

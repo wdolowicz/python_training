c00fcb391912059a5b33f539728c0bc45292dfb9
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-15 21:04:26 +0100

    Added checking for existing contacts and groups

--- test/test_del_contact_hard.py
+++ test/test_del_contact_hard.py
@@ -1,5 +1,8 @@
 __author__ = "wdolowicz"
 
+from model.contact import Contact
 
 def test_del_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(name="test"))
     app.contact.delete_first_contact_hard()
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -5,6 +5,8 @@ from model.contact import Contact
 
 
 def test_mod_contact(app):
-    app.contact.modcontact(Contact(name="Carl", initials="CJ", lname="Johnson", nick="CJ", title="Mr", company="Rockstar Games",
-                                   address="Grove Street", hphone="+199988877715", mail="cj@rockstar.com",
-                                   web="www.rockstargames.com"))
+    if app.contact.count() == 0:
+        app.contact.create(Contact(name="test"))
+    app.contact.modcontact(Contact(name="Carl", initials="CJ", lname="Johnson", nick="CJ", title="Mr",
+                                   company="Rockstar Games", address="Grove Street", hphone="+199988877715",
+                                   mail="cj@rockstar.com", web="www.rockstargames.com"))

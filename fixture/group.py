31c3b99f289d644de02a86873567c2ea76331c4d
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-18 20:10:21 +0100

    Added method for groups check

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,7 @@
 __author__ = 'wdolowicz'
 
+from model.group import Group
+
 
 class GroupHelper:
     def __init__(self, app):
@@ -68,3 +70,14 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
+

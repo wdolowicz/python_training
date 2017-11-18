b67f82a29498a2965e7b8803fbc7263378ceb541
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-18 20:34:19 +0100

    Modified group creation

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,12 +6,16 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="Homework group", header="blabla", footer="blabl"))
+    group = Group(name="Homework group", header="blabla", footer="blabl")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert old_groups == new_groups
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    app.group.create(Group(name="", header="", footer=""))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)

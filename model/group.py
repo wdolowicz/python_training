bafa9da264413e536b2ed3e87771de271977428f
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-18 20:59:03 +0100

    Exported method for id verification

--- model/group.py
+++ model/group.py
@@ -1,5 +1,7 @@
 __author__ = 'wdolowicz'
 
+from sys import maxsize
+
 class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
@@ -11,4 +13,10 @@ class Group:
         return "%s:%s" % (self.id,self.name)
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize

dc110bdb4f07b6c1d665057c9408fc5dccb1fb77
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-18 20:11:05 +0100

    Modified test to check for groups number

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,7 +5,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="Homework group", header="blabla", footer="blabl"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)

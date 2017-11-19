fe47bbbdcb65647482191baccf2470d844995d75
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-19 16:00:52 +0100

    Edited tests to verify lists

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,15 +5,27 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create(Contact(name="John", initials="JD", lname="Doe", nick="JD", title="Mr", company="Microsoft",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(name="John", initials="JD", lname="Doe", nick="JD", title="Mr", company="Microsoft",
                                address="Redmond", hphone="+165688877744", mail="johndoe@microsoft.com",
-                               web="www.johndoe.com"))
+                               web="www.johndoe.com")
+    app.contact.create(Contact())
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_add_empty_contact(app):
-    app.contact.create(Contact(name="", initials="", lname="", nick="", title="", company="",
-                               address="", hphone="", mail="", web=""))
-
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(name="", initials="", lname="", nick="", title="", company="",
+                      address="", hphone="", mail="",
+                      web="")
+    app.contact.create(Contact())
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
--- test/test_mod_contact_hard.py
+++ test/test_mod_contact_hard.py
@@ -7,6 +7,13 @@ from model.contact import Contact
 def test_mod_contact_hard(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    app.contact.modcontact(Contact(name="Carl", initials="CJ", lname="Johnson", nick="CJ", title="Mr",
-                                   company="Rockstar Games", address="Grove Street", hphone="+199988877715",
-                                   mail="cj@rockstar.com", web="www.rockstargames.com"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(name="Carl", initials="CJ", lname="Johnson", nick="CJ", title="Mr",
+                      company="Rockstar Games", address="Grove Street", hphone="+199988877715",
+                      mail="cj@rockstar.com", web="www.rockstargames.com")
+    contact.id = old_contacts[0].id
+    app.contact.modcontact(Contact())
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

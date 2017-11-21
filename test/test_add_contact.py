944cd238eeda219f55a5fd3f40816f0476ab5f4a
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-21 21:25:29 +0100

    Fixed create contact method

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,7 +9,7 @@ def test_add_contact(app):
     contact = Contact(name="John", initials="JD", lname="Doe", nick="JD", title="Mr", company="Microsoft",
                                address="Redmond", hphone="+165688877744", mail="johndoe@microsoft.com",
                                web="www.johndoe.com")
-    app.contact.create(Contact())
+    app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
@@ -21,7 +21,7 @@ def test_add_empty_contact(app):
     contact = Contact(name="", initials="", lname="", nick="", title="", company="",
                       address="", hphone="", mail="",
                       web="")
-    app.contact.create(Contact())
+    app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)

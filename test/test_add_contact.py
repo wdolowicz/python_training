779097c0aff1f5f1d61359d904110713f155721e
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-21 19:13:38 +0100

    Added hashing for groups & contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,8 +10,8 @@ def test_add_contact(app):
                                address="Redmond", hphone="+165688877744", mail="johndoe@microsoft.com",
                                web="www.johndoe.com")
     app.contact.create(Contact())
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -22,8 +22,8 @@ def test_add_empty_contact(app):
                       address="", hphone="", mail="",
                       web="")
     app.contact.create(Contact())
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -13,7 +13,7 @@ def test_mod_contact(app):
                                    mail="cj@rockstar.com", web="www.rockstargames.com")
     contact.id = old_contacts[0].id
     app.contact.modcontact(Contact())
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

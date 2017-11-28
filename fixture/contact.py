9254cb6d941d872fb350c1b3daf46aca466cb3da
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-28 21:37:36 +0100

    Added methods for getting contact info from different pages

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,6 +1,7 @@
 __author__ = 'wdolowicz'
 
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -74,9 +75,6 @@ class ContactHelper:
         self.open_home_page()
         self.contact_cache = None
 
-    def select_contact_to_edit(self, index):
-        wd = self.app.wd
-        wd.find_elements_by_xpath("//table[@id='maintable']/tbody/tr/td[8]/a/img")[index].click()
 
     def modify_first_contact(self):
         wd = self.app.wd
@@ -86,7 +84,7 @@ class ContactHelper:
         wd = self.app.wd
         self.open_home_page()
         # select contact to edit
-        self.select_contact_to_edit(index)
+        self.open_contact_to_edit_by_index(index)
         # edit contact form
         self.fill_contact_form(new_contact_data)
         # submit contact modification
@@ -113,9 +111,18 @@ class ContactHelper:
                 cells = element.find_elements_by_tag_name("td")
                 firstname = cells[2].text
                 lastname = cells[1].text
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(name=firstname, lname=lastname, id=id))
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(name=firstname, lname=lastname, id=id, hphone=all_phones[0],
+                                                  mphone=all_phones[1], wphone=all_phones[2], sphone=all_phones[3]))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
     def open_contact_view_by_index(self, index):
         wd = self.app.wd
@@ -123,3 +130,25 @@ class ContactHelper:
         row = wd.find_elements_by_name("entry")[index]
         cell = row.find_elements_by_tag_name("td")[6]
         cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        name = wd.find_element_by_name("firstname").get_attribute("value")
+        lname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        hphone = wd.find_element_by_name("home").get_attribute("value")
+        mphone = wd.find_element_by_name("mobile").get_attribute("value")
+        wphone = wd.find_element_by_name("work").get_attribute("value")
+        sphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(name=name, lname=lname, id=id, hphone=hphone, mphone=mphone, wphone=wphone, sphone=sphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        hphone = re.search("H: (.*)", text).group(1)
+        mphone = re.search("M: (.*)", text).group(1)
+        wphone = re.search("W: (.*)", text).group(1)
+        sphone = re.search("P: (.*)", text).group(1)
+        return Contact(hphone=hphone, mphone=mphone, wphone=wphone, sphone=sphone)

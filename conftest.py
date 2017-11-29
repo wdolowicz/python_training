cdac411b7056f4e091aa446a8502ba3a0056537f
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-30 00:15:06 +0100

    Parametrized tests to run from commandline

--- conftest.py
+++ conftest.py
@@ -5,17 +5,22 @@ from fixture.application import Application
 
 fixture = None
 
+
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+            fixture.session.login(username="admin", password=password)
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -24,3 +29,9 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--password", action="store", default="secret")

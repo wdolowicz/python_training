11f71ee7531524d31da2133b24324d5a77a81999
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-11-29 21:48:29 +0100

    Added generic data parametrization

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,24 +2,30 @@
 __author__ = "wdolowicz"
 
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(name="", initials="", lname="", nick="", title="", company="", address="", hphone="", mphone="",
+                    wphone="", sphone="", mail="", web="")] + [
+    Contact(name=random_string("name", 10), initials=random_string("initials", 2), lname=random_string("lname", 10),
+            nick=random_string("nick", 10), title=random_string("title", 3), company=random_string("company", 20),
+            address=random_string("address", 30), hphone=random_string("hphone", 9), mphone=random_string("mphone", 9),
+            wphone=random_string("wphone", 9), sphone=random_string("sphone", 9), mail=random_string("mail", 15),
+            web=random_string("web", 20))
+    for i in range(5)
+    ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(name="John", initials="JD", lname="Doe", nick="JD", title="Mr", company="Microsoft",
-                      address="Redmond", hphone="+165688877744", mphone="+1434376655683", wphone="+13475674567456",
-                      sphone="+15643657456485", mail="johndoe@microsoft.com", web="www.johndoe.com")
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(name="", initials="", lname="", nick="", title="", company="", address="", hphone="", mphone="",
-                      wphone="", sphone="", mail="", web="")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()

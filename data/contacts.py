ce5105c83fb0f7dbc80843fc75f91d80d5f84825
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-04 23:41:12 +0100

    Created definition file for generator

---
+++ data/contacts.py
@@ -0,0 +1,12 @@
+__author__ = "wdolowicz"
+
+from model.contact import Contact
+
+
+testdata = [Contact(name="name1", initials="initials1", lname="lname1", nick="nick1", title="title1",
+                    company="company1", address="address1", hphone="hphone1", mphone="mphone1", wphone="wphone1",
+                    sphone="sphone1", mail="mail1", web="web1"),
+            Contact(name="name2", initials="initials2", lname="lname2", nick="nick2", title="title2",
+                    company="company2", address="address2", hphone="hphone2", mphone="mphone2", wphone="wphone2",
+                    sphone="sphone1", mail="mail1", web="web1")
+    ]

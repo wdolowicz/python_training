63742a43dfd5d81d5a2d36c854d3134d2330d638
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-05 00:04:44 +0100

    Modified tests to use json

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,8 +4,8 @@ __author__ = "wdolowicz"
 from model.contact import Contact
 
 
-def test_add_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()

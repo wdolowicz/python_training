e0ae0b08c0d4a8def3b8cfc35d08c0df3e7014da
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-05 00:05:41 +0100

    Refined data generators to use jsonpickle

--- generator/contact.py
+++ generator/contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -42,4 +42,5 @@ testdata = [Contact(name="", initials="", lname="", nick="", title="", company="
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))

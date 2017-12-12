1e6bc7582eb29612990cf1eef6f1dad03ec6becd
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-13 00:06:57 +0100

    Added methods for selecting by id

--- fixture/contact.py
+++ fixture/contact.py
@@ -60,6 +60,10 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def delete_first_contact(self):
         wd = self.app.wd
         self.delete_contact_by_index(0)
@@ -68,6 +72,17 @@ class ContactHelper:
         wd = self.app.wd
         self.open_home_page()
         self.select_contact_by_index(index)
+        # delete
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        # confirm deletion
+        wd.switch_to_alert().accept()
+        self.open_home_page()
+        self.contact_cache = None
+
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_home_page()
+        self.select_contact_by_id(id)
         # delete
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         # confirm deletion

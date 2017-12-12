efc1bc82a2a8ac2c3b24807f9251371703d84ca6
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-13 00:07:27 +0100

    Modified tests to use sql data

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,15 +1,14 @@
 __author__ = "wdolowicz"
-
 from model.contact import Contact
-from random import randrange
+import random
 
-def test_del_some_contact(app):
-    if app.contact.count() == 0:
+def test_del_some_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(name="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
     assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index:index+1] = []
-    assert old_contacts == new_contacts
+    new_contacts = db.get_contact_list()
+    old_contacts.remove(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

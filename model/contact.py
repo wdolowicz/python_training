8c47b088f5c6b1fd055bcc5e7fa69e46f5510cf2
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-14 20:27:18 +0100

    More fixes

--- model/contact.py
+++ model/contact.py
@@ -34,7 +34,7 @@ class Contact:
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
-               and self.firstname == other.name and self.lastname == other.lastname
+               and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:

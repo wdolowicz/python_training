f268b3be8cf37379c389ef4c6fac5b8e0fc3ad08
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-15 00:02:52 +0100

    Testing one, two, three...

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 __author__ = "wdolowicz"
 
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="72"))
     for item in l:
         print(item)
     print(len(l))

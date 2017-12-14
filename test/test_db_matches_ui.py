b18ec3f2204c837d2ac662ad2d6041ea1331ed60
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-15 00:03:23 +0100

    Added method for contacts comparison ui vs db

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,11 +1,19 @@
 __author__ = 'wdolowicz'
 from model.group import Group
-from timeit import timeit
+from model.contact import Contact
 
 
 def test_group_list(app, db):
-    print(timeit(lambda: app.group.get_group_list(), number=1))
+    ui_list = app.group.get_group_list()
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
-    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
-    assert False # sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    db_list = map(clean, db.get_group_list())
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)

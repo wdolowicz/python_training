b2974cc5013715d9bb8715bd2267e709edec6b2f
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-14 20:27:41 +0100

    Added more fields to read from contacts db

--- fixture/db.py
+++ fixture/db.py
@@ -31,10 +31,12 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, email, email2, email3, home, mobile, work, phone2 "
+                           "from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, email, email2, email3, home, mobile, work, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, email=email, email2=email2,
+                                    email3=email3, home=home, mobile=mobile, work=work, secondary=phone2))
         finally:
             cursor.close()
         return list

7baf67737605df41b892961346f37ce4847e8510
Toldo <toldzio@gmail.com>
2018-01-04 15:44:27 +0100

    Added new method for group modification

--- fixture/group.py
+++ fixture/group.py
@@ -89,6 +89,19 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
 
     def return_to_groups_page(self):
         wd = self.app.wd

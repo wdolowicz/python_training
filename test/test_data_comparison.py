4a67c0839f22f1a6c33478d763ae71d51661f596
Toldo <toldzio@gmail.com>
2018-01-08 13:07:54 +0100

    Fixed test

--- test/test_data_comparison.py
+++ test/test_data_comparison.py
@@ -16,17 +16,17 @@ def test_email_on_homepage(app):
 def test_names_and_address_on_homepage(app):
     contact_from_homepage = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_homepage.name == contact_from_edit_page.name
-    assert contact_from_homepage.lname == contact_from_edit_page.lname
+    assert contact_from_homepage.firstname == contact_from_edit_page.firstname
+    assert contact_from_homepage.lastname == contact_from_edit_page.lastname
     assert contact_from_homepage.address == contact_from_edit_page.address
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.hphone == contact_from_edit_page.hphone
-    assert contact_from_view_page.mphone == contact_from_edit_page.mphone
-    assert contact_from_view_page.wphone == contact_from_edit_page.wphone
-    assert contact_from_view_page.sphone == contact_from_edit_page.sphone
+    assert contact_from_view_page.home == contact_from_edit_page.home
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.secondary == contact_from_edit_page.secondary
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -35,10 +35,10 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.hphone, contact.mphone, contact.wphone, contact.sphone]))))
+                                       [contact.home, contact.mobile, contact.work, contact.secondary]))))
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.mail, contact.mail2, contact.mail3]))))
+                                       [contact.email, contact.email2, contact.email3]))))
